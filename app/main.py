8e5242a5aa62a219ee3be0030edbb55cb5b3d941
Rustam Gamidov <rustformail@gmail.com>
2026-02-17 22:20:49 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -6,7 +6,7 @@ class Car:
 
 
 class CarWashStation:
-    def __init__(self, distance_from_city_center: int, clean_power: int,
+    def __init__(self, distance_from_city_center: float, clean_power: int,
                  average_rating: float, count_of_ratings: int) -> None:
         self.distance_from_city_center = distance_from_city_center
         self.clean_power = clean_power
